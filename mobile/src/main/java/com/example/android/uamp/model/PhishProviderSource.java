41a7b0f672aae639cc8a935df96a57a863de677d
Mike Voytovich <mike@currantlabs.com>
2016-07-27 09:34:12 -0700

    add HttpClient fetching code

--- mobile/src/main/java/com/example/android/uamp/model/PhishProviderSource.java
+++ mobile/src/main/java/com/example/android/uamp/model/PhishProviderSource.java
@@ -2,9 +2,13 @@ package com.example.android.uamp.model;
 
 import android.support.v4.media.MediaMetadataCompat;
 import com.example.android.uamp.utils.LogHelper;
+import com.loopj.android.http.*;
+import org.json.*;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by mikevoyt on 7/27/16.
@@ -13,10 +17,27 @@ import java.util.Iterator;
 public class PhishProviderSource implements MusicProviderSource  {
 
     private static final String TAG = LogHelper.makeLogTag(PhishProviderSource.class);
+    ArrayList<String> mYears;
 
     @Override
     public Iterator<MediaMetadataCompat> iterator() {
             ArrayList<MediaMetadataCompat> tracks = new ArrayList<>();
             return tracks.iterator();
+    }
+
+
+    private void fetchYears() {
+        HttpClient.get("years.json", null, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                mYears = ParseUtils.parseYears(response);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+            }
+        });
+
     }
 }
