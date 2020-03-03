b89a011f5428d451c7fc9fae65451f6726f83d3f
Andrew <aj.reitz@gmail.com>
2020-03-03 13:11:17 -0600

    Added Firebase Crashlytics

--- buildSrc/buildSrc.gradle.kts
+++ buildSrc/buildSrc.gradle.kts
@@ -5,12 +5,15 @@ plugins {
 repositories {
     mavenCentral()
     google()
+    maven { url = uri("https://maven.fabric.io/public") }
     jcenter()
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:3.5.2")
+    implementation("com.android.tools.build:gradle:3.5.3")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60")
+    implementation("com.google.gms:google-services:4.3.3")
+    implementation("com.google.firebase:firebase-crashlytics-gradle:2.0.0-beta02")
 }
 
 kotlinDslPluginOptions {
