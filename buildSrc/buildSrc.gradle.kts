78b58100d367b5ab62025e84fbb6b96192fb4214
Andrew Reitz <aj.reitz@gmail.com>
2020-08-20 16:11:18 -0500

    Update Release App Icons
    Minor updates to gradle files.

--- buildSrc/buildSrc.gradle.kts
+++ buildSrc/buildSrc.gradle.kts
@@ -5,15 +5,15 @@ plugins {
 repositories {
     mavenCentral()
     google()
-    maven { url = uri("https://maven.fabric.io/public") }
+    maven("https://maven.fabric.io/public")
     jcenter()
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:3.5.3")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60")
+    implementation("com.android.tools.build:gradle:4.0.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
     implementation("com.google.gms:google-services:4.3.3")
-    implementation("com.google.firebase:firebase-crashlytics-gradle:2.0.0-beta02")
+    implementation("com.google.firebase:firebase-crashlytics-gradle:2.2.0")
 }
 
 kotlinDslPluginOptions {
