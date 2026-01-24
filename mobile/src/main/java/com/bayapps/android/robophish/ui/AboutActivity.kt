12de3f4284de2912d8fdc7c21e5e57c0f7b2d0b6
Mike Voytovich <mike@rokkresearch.com>
2026-01-23 20:00:57 -0800

    add app version info

--- mobile/src/main/java/com/bayapps/android/robophish/ui/AboutActivity.kt
+++ mobile/src/main/java/com/bayapps/android/robophish/ui/AboutActivity.kt
@@ -2,6 +2,7 @@ package com.bayapps.android.robophish.ui
 
 import android.os.Bundle
 import android.webkit.WebView
+import com.bayapps.android.robophish.BuildConfig
 import com.bayapps.android.robophish.R
 
 class AboutActivity : BaseActivity() {
@@ -11,7 +12,14 @@ class AboutActivity : BaseActivity() {
 
         val webView = findViewById<WebView>(R.id.about_webview)
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("file:///android_asset/about.html")
+        webView.settings.defaultTextEncodingName = "utf-8"
+        val rawHtml = assets.open("about.html").bufferedReader().use { it.readText() }
+        val html = rawHtml
+            .replace("{{versionName}}", BuildConfig.VERSION_NAME)
+            .replace("{{versionCode}}", BuildConfig.VERSION_CODE.toString())
+            .replace("{{gitSha}}", BuildConfig.GIT_SHA)
+            .replace("{{buildTime}}", BuildConfig.BUILD_TIME_UTC)
+        webView.loadDataWithBaseURL("file:///android_asset/", html, "text/html", "UTF-8", null)
 
         initializeToolbar()
     }
